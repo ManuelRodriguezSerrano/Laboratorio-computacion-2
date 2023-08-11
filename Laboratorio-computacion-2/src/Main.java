62887dbc102c92fa68b2a8b07f658fb60afd5965
ManuelRodriguezSerrano <manu.rodriguez.serrano@gmail.com>
2023-08-11 12:05:18 -0300

    Agregado los 2 ejer

--- Laboratorio-computacion-2/src/Main.java
+++ Laboratorio-computacion-2/src/Main.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world aburriendome en una clase de java!");
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Hello world, aburrido en una clase de java");
+
+            System.out.println("Ingrese el primer número: ");
+            int num1 = scanner.nextInt();
+
+            System.out.println("Ingrese el segundo número: ");
+            int num2 = scanner.nextInt();
+
+            System.out.println("Ingrese el tercer número: ");
+            int num3 = scanner.nextInt();
+
+            // Ordenar los números de mayor a menor
+            if (num1 >= num2 && num1 >= num3) {
+                if (num2 >= num3) {
+                    System.out.println("Los números ordenados son: " + num1 + ", " + num2 + ", " + num3);
+                } else {
+                    System.out.println("Los números ordenados son: " + num1 + ", " + num3 + ", " + num2);
+                }
+            } else if (num2 >= num1 && num2 >= num3) {
+                if (num1 >= num3) {
+                    System.out.println("Los números ordenados son: " + num2 + ", " + num1 + ", " + num3);
+                } else {
+                    System.out.println("Los números ordenados son: " + num2 + ", " + num3 + ", " + num1);
+                }
+            } else {
+                if (num1 >= num2) {
+                    System.out.println("Los números ordenados son: " + num3 + ", " + num1 + ", " + num2);
+                } else {
+                    System.out.println("Los números ordenados son: " + num3 + ", " + num2 + ", " + num1);
+                }
+            }
+
+            scanner.close();
+        }
     }
-}
